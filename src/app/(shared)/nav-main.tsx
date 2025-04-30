296d9be9608ad80dad66e03acf299471f5f6e8a5
thegurukl83 <thegurukl83@me.com>
2025-04-30 13:30:39 +0100

    fixing navbar social media

--- src/app/(shared)/nav-main.tsx
+++ src/app/(shared)/nav-main.tsx
@@ -3,22 +3,47 @@ import { Button } from "@/components/ui/button";
 import Image from "next/image";
 import Link from "next/link";
 import { useState } from "react";
-import { FaYoutube } from "react-icons/fa";
 import { GrClose } from "react-icons/gr";
 import { SlMenu } from "react-icons/sl";
 import MobileNavbar from "./nav-mobile";
+import { FaYoutube, FaInstagram } from "react-icons/fa";
+import { FaXTwitter } from "react-icons/fa6";
+import { IoLogoWhatsapp } from "react-icons/io";
 
-type NavLink = {
-  text: string;
-  url: string;
+const content = {
+  navLinks: [
+    { text: "Home", url: "/" },
+    { text: "Historia", url: "/story" },
+    { text: "Blog", url: "/blog" },
+    { text: "Patrocinio", url: "/sponsor" },
+  ],
+  socialLinks: [
+    {
+      name: "YouTube",
+      url: "https://www.youtube.com/channel/UCUt1LXPzzrOVe7PyYNYVjFQ",
+      icon: <FaYoutube size={24} />,
+      hoverColor: "#FF0000",
+    },
+    {
+      name: "WhatsApp",
+      url: "https://wa.me/34609254352",
+      icon: <IoLogoWhatsapp size={22} />,
+      hoverColor: "#25D366",
+    },
+    {
+      name: "Instagram",
+      url: "https://www.instagram.com/inmo.inversores",
+      icon: <FaInstagram size={18} />,
+      hoverColor: "#E1306C",
+    },
+    {
+      name: "Twitter",
+      url: "https://x.com/inmoinversores",
+      icon: <FaXTwitter size={18} />,
+      hoverColor: "#1DA1F2",
+    },
+  ],
 };
-
-const navLinks: NavLink[] = [
-  { text: "Home", url: "/" },
-  { text: "Historia", url: "/story" },
-  { text: "Blog", url: "/blog" },
-  { text: "Patrocinio", url: "/sponsor" },
-];
 
 export default function NavMain() {
   const [openNav, setOpenNav] = useState(false);
@@ -26,7 +51,7 @@ export default function NavMain() {
   return (
     <>
       <MobileNavbar
-        navitems={navLinks}
+        navitems={content.navLinks}
         buttons={[
           {
             text: "Ver en YouTube",
@@ -40,64 +65,64 @@ export default function NavMain() {
       />
 
       <nav
-        className={`bg-black text-white py-2 px-0 flex w-full max-w-[100vw]`}
+        className={`bg-gray-900 text-white py-3 flex w-full mx-auto pl-4 pr-1 justify-between items-center`}
       >
-        <div className="flex w-full mx-auto px-4 justify-between items-center">
-          <div className="text-lg font-semibold flex items-center">
-            <Link href="/" className="hover:underline flex items-center">
-              <Image
-                src="/img/logo-navv.png"
-                alt="Logo"
-                width={100}
-                height={50}
-                className="mr-2"
-              />
-            </Link>
-            <ul className="hidden sm:flex text-sm font-medium text-gray-300 ml-8">
-              {navLinks.map((link, index) => (
-                <li key={index} className="">
-                  <Button
-                    variant="link"
-                    asChild
-                    className="hover:no-underline hover:text-white transition duration-300 ease-in-out text-gray-300 font-title text-[17px] font-medium text-xs"
-                  >
-                    <Link href={link.url}>{link.text}</Link>
-                  </Button>
-                </li>
-              ))}
-            </ul>
-          </div>
+        <div className="text-lg font-semibold flex items-center">
+          <Link href="/" className="hover:underline flex items-center">
+            <Image
+              src="/img/logo-navv.png"
+              alt="Logo"
+              width={100}
+              height={50}
+              className="mr-2"
+            />
+          </Link>
+          <ul className="hidden sm:flex text-sm font-medium text-gray-300 ml-8">
+            {content.navLinks.map((link, index) => (
+              <li key={index} className="">
+                <Button
+                  variant="link"
+                  asChild
+                  className="hover:no-underline hover:text-white transition duration-300 ease-in-out text-gray-300 font-title text-[17px] font-medium text-xs"
+                >
+                  <Link href={link.url}>{link.text}</Link>
+                </Button>
+              </li>
+            ))}
+          </ul>
+        </div>
 
-          <div className="flex items-center justify-center gap-4">
-            <Button
-              variant="link"
-              asChild
-              className="hidden  hover:no-underline hover:text-white transition duration-300 ease-in-out text-gray-300 font-title font-medium sm:flex items-center text-xs"
-            >
+        <ul className="flex items-center justify-center gap-4">
+          {content.socialLinks.map((social, index) => (
+            <li key={index}>
               <a
-                href="https://www.youtube.com/channel/UCUt1LXPzzrOVe7PyYNYVjFQ"
+                href={social.url}
                 target="_blank"
                 rel="noopener noreferrer"
+                className={`hover:text-[${social.hoverColor}] transition duration-300 ease-in-out`}
               >
-                <FaYoutube size={20} className="mr-2 text-[#FF0000]" />
-                Ver en YouTube
+                {social.icon}
               </a>
-            </Button>
+            </li>
+          ))}
+          <li>
             <Button
               variant="secondary"
               asChild
-              className="font-medium font-title"
+              className="font-medium font-title px-8"
             >
               <Link href="/course">Ver Curso</Link>
             </Button>
+          </li>
+          <li>
             <Button
               onClick={() => setOpenNav((e) => !e)}
               className="sm:hidden inline-block"
             >
               {openNav ? <GrClose /> : <SlMenu />}
             </Button>
-          </div>
-        </div>
+          </li>
+        </ul>
       </nav>
     </>
   );
